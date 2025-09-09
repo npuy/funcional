0ca0a9acc39da2b789d6f69a6c92e187abcbd325
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-09-09 18:56:38 -0300

    Practico 3: Ejercicio 2

--- practico/p3/p3.hs
+++ practico/p3/p3.hs
@@ -5,3 +5,9 @@ min1 x y = if x < y then x else y
 -- (b) paren x = "("++ show x ++ ")"
 -- paren :: Show a => a -> [Char]
 paren x = "("++ show x ++ ")"
+
+-- 2. Dada la siguiente definici ́on de tipo:
+-- data Semaforo = Verde |Amarillo |Rojo
+-- ¿Qu ́e falta para que sea posible hacer (show Verde)?
+data Semaforo = Verde | Amarillo | Rojo
+    deriving Show
